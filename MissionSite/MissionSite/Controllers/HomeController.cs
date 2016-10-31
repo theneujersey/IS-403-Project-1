9f506862a2741086cf495d58abbe8cc0a02e05ce
jacobnpeterson <jacobnpeterson@gmail.com>
2016-10-31 16:44:53 -0600

    Revert "added mission information in controller"
    This reverts commit 18c84aaa4581a42dae9d32e31afdb2ee6adc02d2.

--- MissionSite/MissionSite/Controllers/HomeController.cs
+++ MissionSite/MissionSite/Controllers/HomeController.cs
@@ -45,49 +45,11 @@ namespace MissionSite.Controllers
         public ViewResult missionFAQs(string Mission)
         {
             if (Mission.Equals("0"))
-            {
                 ViewBag.messageString = "Korea, Busan Mission";
-                ViewBag.president = "Kenneth S. Barrow";
-                ViewBag.address1 = "Korea Busan Mission";
-                ViewBag.address2 = "Dongnae PO Box 73";
-                ViewBag.address3 = "Busan-si";
-                ViewBag.address4 = "Busan-gwangyeoksi 607-600";
-                ViewBag.address5 = "SOUTH KOREA";
-                ViewBag.language = "Korean";
-                ViewBag.climate = "Moderate";
-                ViewBag.religion = "Korean Buddhism";
-                ViewBag.flag = "korea.gif";
-            }
-
             else if (Mission.Equals("1"))
-            {
                 ViewBag.messageString = "Brazil, Rio De Janeiro Mission";
-                ViewBag.president = "Geraldo Lima";
-                ViewBag.address1 = "Brazil Rio De Janeiro Mission";
-                ViewBag.address2 = "Rua Dois de Dezembro 78 salas 703/704";
-                ViewBag.address3 = "Flamengo";
-                ViewBag.address4 = "22220-040 Rio de Janeiro-RJ";
-                ViewBag.address5 = "Brazil";
-                ViewBag.language = "Portuguese";
-                ViewBag.climate = "Hot and humid";
-                ViewBag.religion = "Roman Catholicism";
-                ViewBag.flag = "brazil.png";
-            }
             else if (Mission.Equals("2"))
-            {
                 ViewBag.messageString = "Czech/Slovak Mission";
-                ViewBag.president = "Jan Pohořelický";
-                ViewBag.address1 = "Czech/Slovak Mission";
-                ViewBag.address2 = "Badeniho 1";
-                ViewBag.address3 = "160 00 Praha 6";
-                ViewBag.address4 = "Czech Republic";
-                ViewBag.language = "Czech and Slovak";
-                ViewBag.climate = "Like Utah";
-                ViewBag.religion = "Roman Catholicism";
-                ViewBag.flag = "czech-slovak.jpg";
-            }
-
-                
             else
                 ViewBag.messageString = "Other";
 
